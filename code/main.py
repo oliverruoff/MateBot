8a4af91c9219216eac981298345b40ae162bffb1
Oliver Ruoff <oliver93ruoff@web.de>
2022-08-28 18:31:48 +0200

    removed sleeps

--- code/main.py
+++ code/main.py
@@ -16,17 +16,13 @@ right_stepper = stepper.stepper(
 right_stepper.set_direction_clockwise(False)
 
 left_stepper.turn_stepper_angle(3000, True, True)
-right_stepper.turn_stepper_angle(3000, True, True)
-
-time.sleep(3)
+right_stepper.turn_stepper_angle(3000, False, True)
 
 right_stepper.set_direction_clockwise(True)
 left_stepper.set_direction_clockwise(False)
 
 left_stepper.turn_stepper_angle(3000, True, True)
-right_stepper.turn_stepper_angle(3000, True, True)
-
-time.sleep(3)
+right_stepper.turn_stepper_angle(3000, False, True)
 
 left_stepper.deactivate_stepper()
 right_stepper.deactivate_stepper()
