a6a386852e537d738ac18325ee9e0ec5c180b6d9
Oliver Ruoff <oliver93ruoff@web.de>
2022-09-25 13:24:03 +0200

    test run continuously

--- code/main.py
+++ code/main.py
@@ -15,14 +15,10 @@ right_stepper = stepper.stepper(
 
 right_stepper.set_direction_clockwise(False)
 
-left_stepper.turn_stepper_angle(3000, True, True)
-right_stepper.turn_stepper_angle(3000, False, True)
+left_stepper.run_continuously()
+right_stepper.run_continuously()
 
-right_stepper.set_direction_clockwise(True)
-left_stepper.set_direction_clockwise(False)
-
-left_stepper.turn_stepper_angle(3000, True, True)
-right_stepper.turn_stepper_angle(3000, False, True)
+time.sleep(1)
 
 left_stepper.deactivate_stepper()
 right_stepper.deactivate_stepper()
