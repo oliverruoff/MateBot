b9995b6320e4eb13374a556ae07361ad1efcf447
Oliver Ruoff <oliver93ruoff@web.de>
2022-09-27 21:27:21 +0200

    initialized all motors + test

--- code/main.py
+++ code/main.py
@@ -13,11 +13,27 @@ front_left_stepper = stepper.stepper(
 front_right_stepper = stepper.stepper(
     DIR=11, STEP=9, SLP=10, steps_per_revolution=200, stepper_delay_seconds=0.0005, gpio_mode=GPIO.BCM)
 
-front_right_stepper.turn_stepper_angle(360, False, True)
+back_left_stepper = stepper.stepper(
+    DIR=26, STEP=19, SLP=13, steps_per_revolution=200, stepper_delay_seconds=0.0005, gpio_mode=GPIO.BCM)
 
-front_left_stepper.turn_stepper_angle(360, False, True)
+back_right_stepper = stepper.stepper(
+    DIR=21, STEP=20, SLP=16, steps_per_revolution=200, stepper_delay_seconds=0.0005, gpio_mode=GPIO.BCM)
+
+front_right_stepper.turn_stepper_angle(360, False, False)
+
+front_left_stepper.turn_stepper_angle(360, False, False)
+
+back_left_stepper.turn_stepper_angle(360, False, False)
+
+back_left_stepper.turn_stepper_angle(360, False, False)
+
+# removing holding torque
 
 front_right_stepper.deactivate_stepper()
 
 front_left_stepper.deactivate_stepper()
+
+back_right_stepper.deactivate_stepper()
+
+back_left_stepper.deactivate_stepper()
 
