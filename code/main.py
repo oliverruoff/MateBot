69aa29201271cb84467786aac865c9a57dcbff0f
Oliver Ruoff <oliver93ruoff@web.de>
2022-09-27 21:21:27 +0200

    test front steppers

--- code/main.py
+++ code/main.py
@@ -7,13 +7,17 @@ import RPi.GPIO as GPIO
 # Initializing sensors, motors, etc.
 ####################################
 
-left_stepper = stepper.stepper(
+front_left_stepper = stepper.stepper(
     DIR=4, STEP=3, SLP=2, steps_per_revolution=200, stepper_delay_seconds=0.0005, gpio_mode=GPIO.BCM)
 
-right_stepper = stepper.stepper(
+front_right_stepper = stepper.stepper(
     DIR=11, STEP=9, SLP=10, steps_per_revolution=200, stepper_delay_seconds=0.0005, gpio_mode=GPIO.BCM)
 
-right_stepper.turn_stepper_angle(360, False, True)
+front_right_stepper.turn_stepper_angle(360, False, True)
 
-right_stepper.deactivate_stepper()
+front_left_stepper.turn_stepper_angle(360, False, True)
+
+front_right_stepper.deactivate_stepper()
+
+front_left_stepper.deactivate_stepper()
 
