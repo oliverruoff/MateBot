267e8b06c9f58cf716e069c14fc7c23a46f4dccf
Oliver Ruoff <oliver93ruoff@web.de>
2022-10-01 14:02:35 +0200

    implemented robot class. testing first drive

---
+++ code/bot/robot.py
@@ -0,0 +1,70 @@
+
+class Robot:
+
+    def __init__(self, front_left_stepper, front_right_stepper, back_left_stepper, back_right_stepper):
+        self.front_left_stepper = front_left_stepper
+        self.front_right_stepper = front_right_stepper
+        self.back_left_stepper = back_left_stepper
+        self.back_right_stepper = back_right_stepper
+
+        self.position_X = 0
+        self.position_Y = 0
+        self.heading = 0
+
+        self.TYRE_CIRCUMFERENCE_CM = 18.85              # manually determined
+        self.ROBOT_CIRCUMFERENCE_CM = 157               # manually determined
+        self.TURNING_ERROR_MULTIPLIER = 0               # manually determined
+
+    def turn_all_steppers_angle(self, degree, asynch, ramping):
+        if asynch:
+            self.front_left_stepper.turn_stepper_angle(
+                degree, True, ramping)
+            self.front_right_stepper.turn_stepper_angle(
+                degree, True, ramping)
+            self.back_left_stepper.turn_stepper_angle(
+                degree, True, ramping)
+            self.back_right_stepper.turn_stepper_angle(
+                degree, True, ramping)
+        else:
+            self.front_left_stepper.turn_stepper_angle(
+                degree, True, ramping)
+            self.front_right_stepper.turn_stepper_angle(
+                degree, True, ramping)
+            self.back_left_stepper.turn_stepper_angle(
+                degree, True, ramping)
+            self.back_right_stepper.turn_stepper_angle(
+                degree, False, ramping)
+
+    def drive_cm(self, cm, forward, ramping=False):
+        if forward:
+            self.front_left_stepper.set_direction_clockwise(False)
+            self.front_right_stepper.set_direction_clockwise(True)
+            self.back_left_stepper.set_direction_clockwise(False)
+            self.back_right_stepper.set_direction_clockwise(True)
+        else:
+            self.front_left_stepper.set_direction_clockwise(True)
+            self.front_right_stepper.set_direction_clockwise(False)
+            self.back_left_stepper.set_direction_clockwise(True)
+            self.back_right_stepper.set_direction_clockwise(False)
+        desired_angle = (cm / self.TYRE_CIRCUMFERENCE_CM) * \
+            360 * self.TURNING_ERROR_MULTIPLIER
+
+        self.turn_all_steppers_angle(desired_angle, False, ramping)
+
+    def turn_degree(self, degree, clockwise, ramping=False):
+        if clockwise:
+            self.front_left_stepper.set_direction_clockwise(False)
+            self.front_right_stepper.set_direction_clockwise(False)
+            self.back_left_stepper.set_direction_clockwise(False)
+            self.back_right_stepper.set_direction_clockwise(False)
+        else:
+            self.front_left_stepper.set_direction_clockwise(True)
+            self.front_right_stepper.set_direction_clockwise(True)
+            self.back_left_stepper.set_direction_clockwise(True)
+            self.back_right_stepper.set_direction_clockwise(True)
+
+        desired_angle = (self.ROBOT_CIRCUMFERENCE_CM /
+                         self.TYRE_CIRCUMFERENCE_CM) * degree * self.TURNING_ERROR_MULTIPLIER
+        self.turn_all_steppers_angle(desired_angle, False, ramping)
+        self.heading = (self.heading +
+                        degree) % 360 if clockwise else (self.heading - degree) % -360
--- code/main.py
+++ code/main.py
@@ -1,57 +1,45 @@
 from actuators import stepper
+from bot import robot
 
 import time
 import RPi.GPIO as GPIO
 
-####################################
-# Initializing sensors, motors, etc.
-####################################
+try:
 
-front_left_stepper = stepper.stepper(
-    DIR=4, STEP=3, SLP=2, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
+    
+    ####################################
+    # Initializing sensors, motors, etc.
+    ####################################
+    front_left_stepper = stepper.stepper(
+        DIR=4, STEP=3, SLP=2, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
 
-front_right_stepper = stepper.stepper(
-    DIR=11, STEP=9, SLP=10, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
+    front_right_stepper = stepper.stepper(
+        DIR=11, STEP=9, SLP=10, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
 
-back_left_stepper = stepper.stepper(
-    DIR=26, STEP=19, SLP=13, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
+    back_left_stepper = stepper.stepper(
+        DIR=26, STEP=19, SLP=13, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
 
-back_right_stepper = stepper.stepper(
-    DIR=21, STEP=20, SLP=16, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
+    back_right_stepper = stepper.stepper(
+        DIR=21, STEP=20, SLP=16, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.000005, gpio_mode=GPIO.BCM)
 
-front_left_stepper.set_stepper_mode('1/32')
-front_right_stepper.set_stepper_mode('1/32')
-back_left_stepper.set_stepper_mode('1/32')
-back_right_stepper.set_stepper_mode('1/32')
+    robo = robot.Robot(front_left_stepper, front_right_stepper, back_left_stepper, back_right_stepper)
 
-front_left_stepper.set_direction_clockwise(False)
-back_left_stepper.set_direction_clockwise(False)
+    
+    robo.drive_cm(50, True)
+    robo.turn_degree(90, True)
+    robo.drive_cm(50, True)
+    robo.turn_degree(90, True)
+    robo.drive_cm(50, True)
+    robo.turn_degree(90, True)
+    robo.drive_cm(50, True)
+    robo.turn_degree(90, True)
 
-#front_right_stepper.run_continuously(frequency=500)
-#front_left_stepper.run_continuously(frequency=500)
-#back_right_stepper.run_continuously(frequency=500)
-#back_left_stepper.run_continuously(frequency=500)
+    
+except KeyboardInterrupt:
 
-#time.sleep(2)
-
-front_right_stepper.turn_stepper_angle(360, True, False)
-front_left_stepper.turn_stepper_angle(360, True, False)
-back_right_stepper.turn_stepper_angle(360, True, False)
-back_left_stepper.turn_stepper_angle(360, False, False)
-
-front_right_stepper.stop_continuous()
-front_left_stepper.stop_continuous()
-back_right_stepper.stop_continuous()
-back_left_stepper.stop_continuous()
-
-
-# removing holding torque
-
-front_right_stepper.deactivate_stepper()
-
-front_left_stepper.deactivate_stepper()
-
-back_right_stepper.deactivate_stepper()
-
-back_left_stepper.deactivate_stepper()
+    # removing holding torque
+    front_right_stepper.deactivate_stepper()
+    front_left_stepper.deactivate_stepper()
+    back_right_stepper.deactivate_stepper()
+    back_left_stepper.deactivate_stepper()
 
