0ec92e27c84667952182df714c6650995b3d4d17
Oliver Ruoff <oliver93ruoff@web.de>
2022-10-01 12:11:23 +0200

    Full stepper test

--- code/main.py
+++ code/main.py
@@ -19,20 +19,22 @@ back_left_stepper = stepper.stepper(
 back_right_stepper = stepper.stepper(
     DIR=21, STEP=20, SLP=16, M0=7, M1=8, M2=25, steps_per_revolution=200, stepper_delay_seconds=0.0005, gpio_mode=GPIO.BCM)
 
-front_left_stepper.set_stepper_mode('1/16')
-front_right_stepper.set_stepper_mode('1/16')
-back_left_stepper.set_stepper_mode('1/16')
-back_right_stepper.set_stepper_mode('1/16')
+#front_left_stepper.set_stepper_mode('1/16')
+front_right_stepper.set_stepper_mode('Full')
+#back_left_stepper.set_stepper_mode('1/16')
+#back_right_stepper.set_stepper_mode('1/16')
 
-front_left_stepper.set_direction_clockwise(False)
-back_left_stepper.set_direction_clockwise(False)
+#front_left_stepper.set_direction_clockwise(False)
+#back_left_stepper.set_direction_clockwise(False)
 
-front_right_stepper.run_continuously(frequency=500)
-front_left_stepper.run_continuously(frequency=500)
-back_right_stepper.run_continuously(frequency=500)
-back_left_stepper.run_continuously(frequency=500)
+#front_right_stepper.run_continuously(frequency=500)
+#front_left_stepper.run_continuously(frequency=500)
+#back_right_stepper.run_continuously(frequency=500)
+#back_left_stepper.run_continuously(frequency=500)
 
-time.sleep(2)
+#time.sleep(2)
+
+front_right_stepper.turn_stepper_angle(360, False, False)
 
 front_right_stepper.stop_continuous()
 front_left_stepper.stop_continuous()
