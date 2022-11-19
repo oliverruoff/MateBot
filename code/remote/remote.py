0efe3606d947cc00484662fb00a994c3df899cac
Oliver Ruoff <oliver93ruoff@web.de>
2022-11-20 00:12:33 +0100

    initial remote version

--- code/remote/remote.py
+++ code/remote/remote.py
@@ -1,26 +1,39 @@
 import tkinter as tk
-import urllib.request
+import requests
 
-SERVER_ADDRESS = "matebot.local:5000/"
+SERVER_ADDRESS = "http://matebot.local:5000/"
 
-def up_up():
-    print('up up')
+def up_up(event):
+    result = requests.get(url = SERVER_ADDRESS + "stop")
+    print(result.content)
 
-def up_down():
-    contents = urllib.request.urlopen(SERVER_ADDRESS + "health").read()
-    print(contents)
+def up_down(event):
+    result = requests.get(url = SERVER_ADDRESS + "move", params = {'forward': True})
+    print(result.content)
 
 def down_down(event):
-    print('down down', event)
+    result = requests.get(url = SERVER_ADDRESS + "move", params = {'forward': False})
+    print(result.content)
 
 def down_up(event):
-    print('down up', event)
+    result = requests.get(url = SERVER_ADDRESS + "stop")
+    print(result.content)
 
-def left():
-    pass
+def left_down(event):
+    result = requests.get(url = SERVER_ADDRESS + "turn", params = {'direction': 'left'})
+    print(result.content)
 
-def right():
-    pass
+def left_up(event):
+    result = requests.get(url = SERVER_ADDRESS + "stop")
+    print(result.content)
+
+def right_down(event):
+    result = requests.get(url = SERVER_ADDRESS + "turn", params = {'direction': 'right'})
+    print(result.content)
+
+def right_up(event):
+    result = requests.get(url = SERVER_ADDRESS + "stop")
+    print(result.content)
 
 root = tk.Tk()
 
@@ -30,11 +43,17 @@ button_left = tk.Button(root, text="◄")
 button_right = tk.Button(root, text="►")
 
 button_up.grid(row=0, column=1, padx='2', pady='2', sticky='ew')
+button_up.bind('<ButtonPress-1>', up_down)
+button_up.bind('<ButtonRelease-1>', up_up)
 button_down.grid(row=1, column=1, padx='2', pady='2', sticky='ew')
 button_down.bind('<ButtonPress-1>', down_down)
 button_down.bind('<ButtonRelease-1>', down_up)
 button_left.grid(row=1, column=0, padx='2', pady='2', sticky='ew')
+button_left.bind('<ButtonPress-1>', left_down)
+button_left.bind('<ButtonRelease-1>', left_up)
 button_right.grid(row=1, column=2, padx='2', pady='2', sticky='ew')
+button_right.bind('<ButtonPress-1>', right_down)
+button_right.bind('<ButtonRelease-1>', right_up)
 
 
 root.mainloop()
