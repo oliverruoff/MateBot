9d37d15c36490414f77871de9af130d3a08e2665
Oliver Ruoff <oliver93ruoff@web.de>
2022-12-17 17:56:41 +0100

    corrected file name

--- code/object_detection/detection.py
+++ code/object_detection/detection.py
@@ -13,7 +13,7 @@ class Detector:
 
     # Initialize the object detection model
     base_options = core.BaseOptions(
-        file_name='/home/pi/develop/MateBot/code/object_detection/model/efficientdet_lite0.tflite', use_coral=False, num_threads=4)
+        file_name='object_detection/model/efficientdet_lite0.tflite', use_coral=False, num_threads=4)
     detection_options = processor.DetectionOptions(
         max_results=3, score_threshold=0.3)
     options = vision.ObjectDetectorOptions(
