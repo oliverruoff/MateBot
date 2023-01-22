76e27685cb0afd28ecb0569526af5cd424c56848
Oliver Ruoff <oliver93ruoff@web.de>
2023-01-22 18:15:45 +0100

    test save pic

--- code/robot_server.py
+++ code/robot_server.py
@@ -114,10 +114,10 @@ def save_picture():
     # dateTimeObj = datetime.now()
     # timestampStr = dateTimeObj.strftime("%Y-%m-%dT%H-%M-%S")
     folder = 'saved_pictures'
-    filename = os.path.join(app.root_path, folder, 'tmp'  + '.jpg')
+    filename = os.path.join(folder, 'tmp'  + '.jpg')
     cv2.imwrite(filename, current_camera_picture_as_jpeg)
     print('Saved picture: ', filename)
-    return send_file(filename, as_attachment=True)
+    return ""#send_file(filename, as_attachment=True)
 
 
 @app.route('/video_feed')
