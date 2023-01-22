286e7b89fd1cbc1637c4ff47ee9fa5ec0ae24dd1
Oliver Ruoff <oliver93ruoff@web.de>
2023-01-22 18:58:02 +0100

    readded caching

--- code/robot_server.py
+++ code/robot_server.py
@@ -16,7 +16,6 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 html_template_dir = os.path.join(dir_path, 'server')
 
 app = Flask(__name__, template_folder=html_template_dir)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 cam = camera.camera()
 
@@ -121,18 +120,6 @@ def save_picture():
     cv2.imwrite(abs_file, current_camera_picture_as_jpeg)
     print('Saved picture: ', abs_file)
     return send_file(abs_file, as_attachment=True)
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
 
 @app.route('/video_feed')
 def video_feed():
